e5d87e26b26686ccd9abb761b0ba72e5204b5c96
Aniket Arjun Khillare <aniketkhillare17@gmail.com>
2025-07-04 11:09:31 +0530

    testing cicd 2

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -22,7 +22,7 @@ const AdminDashboard = () => {
         {/* Header */}
         <div>
           <h1 className="text-2xl font-bold text-gray-900">Dashboard</h1>
-          <p className="mt-1 text-sm text-gray-500">School Management</p>
+          <p className="mt-1 text-sm text-gray-500">School Management 1</p>
         </div>
 
         {/* Statistics Cards */}
